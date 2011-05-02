5fe0abf71e2fa0f081d6575348d6614ad4ece7d3
Benjamin Blundell <oni@section9.co.uk>
2011-05-02 16:09:25 +0100

    Added 2D curves

--- js/s9WebGL.js
+++ js/s9WebGL.js
@@ -17,7 +17,9 @@ Benjamin Blundell
 oni @ section9.co.uk
 
 http://www.section9.co.uk
-http://www.casa.ucl.ac.uk
+
+Uses Sylvester
+http://sylvester.jcoglan.com/
 
 Based on the excellent work by Giles over at http://learningwebgl.com/
 
@@ -32,7 +34,7 @@ http://creativecommons.org/licenses/by-nc-sa/3.0/
 
 var gl;
 
-var DEBUG = true;
+var DEBUG = false;
 
 // Debug mode does seem to have some side-effects!
 
@@ -534,6 +536,7 @@ function Primitive() {
     this.vertexTextureCoordBuffer   = gl.createBuffer();
     this.vertexColorBuffer          = gl.createBuffer();
     this.vertexIndexBuffer          = gl.createBuffer();
+    this.drawPrimitive				= gl.TRIANGLES;
     
     this.draw = function () {
     
@@ -560,12 +563,11 @@ function Primitive() {
                 gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexColorBuffer);
                 gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, this.vertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
             }
-            
-          
-            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
-            setMatrixUniforms(shaderProgram);
-            gl.drawElements(gl.TRIANGLES, this.vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
         }
+        
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
+        setMatrixUniforms(shaderProgram);
+        gl.drawElements(this.drawPrimitive, this.vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT,0);
     }
 }
 
@@ -823,7 +825,7 @@ function createSphere(latitudeBands,longitudeBands) {
     sphere.vertexPositionBuffer.numItems = vertexPositionData.length / 3;
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sphere.vertexIndexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STREAM_DRAW);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STATIC_DRAW);
     sphere.vertexIndexBuffer.itemSize = 1;
     sphere.vertexIndexBuffer.numItems = indexData.length;
     
@@ -911,6 +913,105 @@ function loadModel(path) {
 }
 
 
+// ****************************************************
+// Drawing Curves
+// ****************************************************
+
+function B1(t) { return t*t*t }
+function B2(t) { return 3*t*t*(1-t) }
+function B3(t) { return 3*t*(1-t)*(1-t) }
+function B4(t) { return (1-t)*(1-t)*(1-t) }
+
+// val is 0-1
+function pointOnCurve(val,v0,v1,v2,v3) {
+  var pos = $V([
+  v0.e(1)*B1(val) + v1.e(1)*B2(val) + v2.e(1)*B3(val) + v3.e(1)*B4(val),
+  v0.e(2)*B1(val) + v1.e(2)*B2(val) + v2.e(2)*B3(val) + v3.e(2)*B4(val),
+  v0.e(3)*B1(val) + v1.e(3)*B2(val) + v2.e(3)*B3(val) + v3.e(3)*B4(val)]);
+  
+  return pos;
+}
+
+function createCurve(start, end, c0, c1, facing, segments, w, r,g,b,a) {
+
+    var curve = new Primitive;
+   	var alignAxis = facing;
+   	var width = w;
+	
+	// Create the Vertices for this curve as a triangle strip facing the 
+	// align axis vector (may need to mess with this in a shader to keep it
+	// facing the viewer (billboarding)
+   
+    gl.bindBuffer(gl.ARRAY_BUFFER, curve.vertexPositionBuffer);
+    
+  	var vertices = [];
+  
+  	// Direction gives the next point - this is point segment 0 to 1
+
+  	var vert0 = pointOnCurve(0, start, c0,c1, end); // First point
+  	var vert3 = pointOnCurve(1.0 / segments, start, c0,c1, end);
+  	var dir = vert0.subtract(vert3); // Going back
+  	var up = dir.cross(alignAxis);
+  	up = up.multiply(1.0/up.modulus() * width);
+ 
+  	var vert1 = vert0.add(up); // going up to second
+  	var vert2 = vert3.add(up); // across to third point
+  	
+  	vertices.push(vert1.e(1)); vertices.push(vert1.e(2)); vertices.push(vert1.e(3));
+  	vertices.push(vert0.e(1)); vertices.push(vert0.e(2)); vertices.push(vert0.e(3));
+	vertices.push(vert2.e(1)); vertices.push(vert2.e(2)); vertices.push(vert2.e(3));
+	vertices.push(vert3.e(1)); vertices.push(vert3.e(2)); vertices.push(vert3.e(3));
+
+   	for (var i=2; i < segments; i ++){
+		vert0 = pointOnCurve((i-1)/segments, start, c0,c1,end);
+		vert3 = pointOnCurve(i/segments, start, c0,c1,end);
+		var dir = vert0.subtract(vert3); // Going back
+		var up = dir.cross(alignAxis);
+		up = up.multiply(1.0/up.modulus() * width);
+		
+  		vert2 = vert3.add(up); // across to third point
+  	
+		vertices.push(vert2.e(1)); vertices.push(vert2.e(2)); vertices.push(vert2.e(3));
+		vertices.push(vert3.e(1)); vertices.push(vert3.e(2)); vertices.push(vert3.e(3));
+		
+
+	}
+  	   
+    
+    var colors = [];
+    
+  	for (var i=0; i < segments * 2; i ++){
+   		colors.push(r);
+   		colors.push(g);
+   		colors.push(b);
+   		colors.push(a);
+   	}
+   	
+   	var curveVertexIndices = [];
+
+   	for (var i=0; i < segments * 2; i ++){
+   		curveVertexIndices.push(i);
+   	}
+   
+ 	gl.bindBuffer(gl.ARRAY_BUFFER, curve.vertexPositionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    curve.vertexPositionBuffer.itemSize = 3;
+    curve.vertexPositionBuffer.numItems = vertices.length / 3;
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, curve.vertexColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    curve.vertexColorBuffer.itemSize = 4;
+    curve.vertexColorBuffer.numItems = colors.length / 4;
+    
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, curve.vertexIndexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(curveVertexIndices), gl.STATIC_DRAW);
+    curve.vertexIndexBuffer.itemSize = 1;
+    curve.vertexIndexBuffer.numItems = curveVertexIndices.length;
+ 
+	curve.drawPrimitive = gl.TRIANGLE_STRIP;
+
+    return curve;
+}
 
 
 
